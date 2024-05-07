9253ecf861ef60e2337a5899b81ea8e063b0a783
Yash Meena <yashmeena1010@gmail.com>
2024-05-07 15:51:32 +0530

    adding the detail pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
             <Route path="/sign-up" element={<SignUp/>}></Route>
             <Route path="/sign-in" element={<SignIn/>}></Route>
             <Route path="/about" element={<About/>}></Route>
+            <Route path="/listing/:listingId" element={<Listing/>}></Route>
                 
             <Route element={<PrivateRoute/>}>
                  <Route path="/profile" element={<Profile />} />
