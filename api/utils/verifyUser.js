b276f138da784c951b69213041b0b689dea8dd42
Yash Meena <yashmeena1010@gmail.com>
2024-05-25 17:12:02 +0530

    complete the search functionality with controller

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -6,7 +6,7 @@ export const verifyToken=(req,res,next)=>{
       const token=req.cookies['access_token'];
 
       if(!token) return next(errorHandler(401,"Unauthorized!"));
-
+//the user in (err,user) comes from the jet.verify it is onof the property of it in user the information of user is stored (payload i.e _id)
       jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
             if(err) return next(errorHandler(403,'Forbidden'));
 
